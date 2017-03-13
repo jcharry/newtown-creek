fd1c4223a81b88f109e04f217e6745be59d798a5
jcharry <jamie.charry@gmail.com>
2017-03-12 22:04:56 -0400

    scroll to top on router update

--- app/router/index.jsx
+++ app/router/index.jsx
@@ -1,8 +1,7 @@
+/* global window */
 import React from 'react';
 import useRouterHistory from 'react-router/lib/useRouterHistory';
-//import createHashHistory from 'react-router/node_modules/history/lib/createHashHistory';
 import createBrowserHistory from 'react-router/node_modules/history/lib/createBrowserHistory';
-//
 import Route from 'react-router/lib/Route';
 import IndexRoute from 'react-router/lib/IndexRoute';
 import Router from 'react-router/lib/Router';
@@ -13,7 +12,7 @@ import PamphletContainer from 'app/components/ContentPage/PamphletContainer';
 const appHistory = useRouterHistory(createBrowserHistory)({ queryKey: false });
 
 export default (
-    <Router history={appHistory}>
+    <Router onUpdate={() => window.scrollTo(0, 0)} history={appHistory}>
         <Route path='/'>
             <Route path='/:page' component={PamphletContainer} />
             <IndexRoute component={Landing} />
