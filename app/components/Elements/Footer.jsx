6e8926b5f46903714d72c0891a3ad7c7b9b6b235
jcharry <jamie.charry@gmail.com>
2017-08-16 23:10:41 -0400

    add footer

---
+++ app/components/Elements/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ExternalLink from 'app/components/elements/ExternalLink';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className='page-footer'>
+                <ExternalLink url='https://www.facebook.com/FloatingStudioforDarkEcologies'>Like us on Facebook to hear about upcoming events</ExternalLink>
+                <a href='mailto: newtowncreekfieldguide@gmail.com'>Contact us here</a>
+            </div>
+
+        );
+    }
+}
+
+export default Footer;
